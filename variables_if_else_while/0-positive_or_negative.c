ac081664eae09b213ecadeaafa0a7528ab40a776
System Administrator <root@mbp-de-ines.home>
2024-02-20 19:21:47 +0100

    update task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,18 +15,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* my code */
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("n is negative\n");
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("n is zero\n");
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
