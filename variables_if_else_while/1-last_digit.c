2147ec0edcf7612eec090b61d888c687b6fcb713
System Administrator <root@mbp-de-ines.home>
2024-02-21 10:46:20 +0100

    update task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,15 +23,15 @@ int main(void)
 	/* print the last digit */
 	if (LastDigit > 5)
 	{
-		printf("Last digit of %d is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 	}
 	else if (LastDigit == 0)
 	{
-		printf("Last digit of %d is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, LastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
 	}
 	return (0);
 }
