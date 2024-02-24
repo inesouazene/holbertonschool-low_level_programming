8518e7b0bbc069d1126e6bbd9a56093bf0153153
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-02-24 13:29:01 +0100

    first commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: always 0
+*/
+
+
+int main(void)
+{
+/* use of _putchar function to print '_putchar' */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+return (0);
+
+}
