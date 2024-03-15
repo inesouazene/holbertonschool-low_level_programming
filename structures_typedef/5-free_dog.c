edd9015558d07e29f758d91e1448a72379507139
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-15 19:15:14 +0100

    add task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dog
+ * @d: dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
