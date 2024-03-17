95d498df08dae3c4f406b799b2af7e1643464383
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-17 21:41:50 +0100

    fix task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,18 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -22,19 +33,5 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-
-
-	}
 	printf("\n");
-
 }
