72e0b04614b39bfed424ae54de81e543787187d9
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-17 18:31:20 +0100

    add task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _abs - function that computes absolute value of an int
+* @n: int to check
+* Return: n if n greater or equal to 0
+* otherwise multiply n by -1 to get the absolute value of n because - * - = +
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
+
+/**
+* print_last_digit - function that  prints the last digit of a number.
+*
+* Return: the value of the last digit
+*/
+
+int print_last_digit(int num)
+{
+	_putchar('0' + _abs(num % 10));
+	return (_abs(num % 10));
+}
