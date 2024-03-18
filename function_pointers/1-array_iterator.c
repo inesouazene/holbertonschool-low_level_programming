fe096065bbc51ae90e504ad6f1337a6810380eeb
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-18 22:05:29 +0100

    fix task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
-* print_name - function that executes a function given as a parameter on each element of an array.
+* array_iterator - function that executes a function given as a parameter
+* on each element of an array.
 * @array: array to explore
 * @size: size of the array
 * @action: pointer to the used function
@@ -9,10 +10,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+
+	if (array != NULL || size > 0 || action != NULL)
+	{
 		size_t index;
 
 		for (index = 0; index < size; index++)
 		{
 			action(array[index]);
 		}
+	}
 }
