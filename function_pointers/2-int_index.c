9be5d8ada569b4b4213898f5abf05ac20b9686f4
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-18 22:26:32 +0100

    fix task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,17 +12,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int index;
 
-	if (array != NULL || size > 0 || cmp != NULL)
-	{
-		int index;
-
-		for (index = 0; index < size; index++)
-		{
-			if (cmp(array[index]) != 0)
-				return (index);
-		}
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
 	}
 	return (-1);
 }
