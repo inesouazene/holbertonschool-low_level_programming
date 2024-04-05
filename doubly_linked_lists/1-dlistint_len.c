5b4808953ab95610a37f39cce66a5a3b471489d3
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-05 22:24:23 +0200

    add task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+*
+* @h: head of list
+*
+* Return: number of elements in a linked list_t list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp;
+	int elements_count = 0;
+
+	tmp = h;
+
+	while (tmp != NULL)
+	{
+		elements_count++;
+		tmp = tmp->next;
+	}
+
+	return (elements_count);
+}
