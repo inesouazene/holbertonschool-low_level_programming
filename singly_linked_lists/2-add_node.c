93b3341352b0a4db92f15d9fb97e973b8a6c141b
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-05 16:23:16 +0200

    add task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/*
+* add_node - Adds a new node at the beginning of a list_t list
+*
+* @head: Pointer to a pointer to the head of the list
+* @str: String value to be stored in the new node
+*
+* Return: Pointer to the newly added node or NULL if memory allocation fails
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int length = 0;
+
+	while (str[length])
+	{
+		length++;
+	}
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = length;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
