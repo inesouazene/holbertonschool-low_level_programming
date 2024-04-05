37c058b6566d5a9610ea9f0537975445ec176d17
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-05 16:40:37 +0200

    update task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,29 +1,40 @@
 #include "lists.h"
 
-/*
- * add_node_end - function that adds a new node at the end of a list_t list
- * @head: Pointer to a pointer to the head of the list
- * @str: String value to be stored in the new node
- *
- * Return: Pointer to the newly added node or NULL if memory allocation fails
- */
+/**
+* _strlen - function that returns the length of a string
+* @s: string to get length
+* Return: length of the string
+*/
+
+int _strlen(const char *s)
+{
+	int num_char;
+
+	for (num_char = 0; s[num_char] != '\0'; num_char++)
+	{
+	}
+	return (num_char);
+}
+
+/**
+* add_node_end - function that adds a new node at the end of a list_t list
+* @head: Pointer to a pointer to the head of the list
+* @str: String value to be stored in the new node
+*
+* Return: Pointer to the newly added node or NULL if memory allocation fails
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int index = 0;
 	list_t *new, *last_node;
 
-	while (str[index])
-	{
-		index++;
-	}
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = index;
+	new->len = _strlen(str);
 	new->next = NULL;
 	if (strdup(str) == NULL)
 	{
