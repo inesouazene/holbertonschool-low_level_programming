0e50514f263474089ebc170ad1f82d0c02af324b
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-06 21:32:13 +0200

    fix task 3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,27 +1,5 @@
 #include "main.h"
 
-#include "main.h"
-/**
- * get_bit - function that returns the value
- * of a bit at a given index.
- * @n: number to look at
- * @index: index starting from 0, of the bit we want to get
- *
- * Return: the value of the bit at index index or -1 if an error occured
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	unsigned long int shifted_number;
-
-	if (index > 64)
-		return (-1);
-
-	shifted_number = n >> index;
-
-	return (shifted_number & 1);
-}
-
-
 /**
  * set_bit - Function that sets the value
  * of a bit to 1 at a given index.
@@ -32,10 +10,16 @@ int get_bit(unsigned long int n, unsigned int index)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 32)
+	unsigned long int position;
+
+	if (index > 64)
 		return (-1);
-	(*n) |= 1 << index;
-	if (get_bit((*n), index) == 1)
-		return (1);
-	return (-1);
+
+	for (position = 1; index > 0; index--, position *= 2)
+	{
+
+	}
+	*n += position;
+
+	return (1);
 }
