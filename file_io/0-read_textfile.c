4b97fb13a81a2d0b8652e967fe7cbc296f1f683e
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-06 23:26:36 +0200

    add first files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *content;
+
+	if (filename == NULL)
+		return (0);
+
+	content = malloc(sizeof(char) * letters);
+	if (content == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, content, letters);
+	w = write(STDOUT_FILENO, content, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(content);
+		return (0);
+	}
+
+	free(content);
+	close(o);
+
+	return (w);
+}
