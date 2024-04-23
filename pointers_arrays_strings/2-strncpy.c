e4822b57d1ece27cf5f85ec5e186b221888bec47
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-23 23:37:35 +0200

    add task 2 strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
+}
